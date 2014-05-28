074388ea6415ebbb5c8b4c786be692a6e7146d41
bkucey <brennan.kucey@gmail.com>
2014-05-27 21:38:08 -0700

    Set up some tests

---
+++ NaiveBayes/test/PreProcessorTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PreProcessorTest extends PreProcessor{
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPreProcess(){
+		//load in the stop words
+		//load in the messages 
+		//get a set of all words in messages that aren't stop words
+		//count the length and make a vector description 
+		//loop over all the messages and create their respective feature vectors 
+		
+		
+		//load in the test messages 
+		//convert each of the messages to a feature of "activeWords"
+	}
+
+}
